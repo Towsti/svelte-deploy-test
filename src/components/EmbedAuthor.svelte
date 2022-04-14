e53d9d59febcc645774be9a6ef80d5c081e56d0a
Towsti <bastiaanmetz+1@gmail.com>
2022-04-14 17:05:18 +0200

    first embed components

---
+++ src/components/EmbedAuthor.svelte
@@ -0,0 +1,20 @@
+<!-- <script>
+    export let name, url, icon_url;
+    let authorName;
+
+    $: if (name) {
+        if (url) {
+            authorName = <Link href={url} className='embed-author-name'>{name}</Link>;
+        }
+    }
+
+    function extractRGB(i) {
+        return {
+            r: (i >> 16) & 0xFF,
+            g: (i >> 8) & 0xFF,
+            b: i & 0xFF,
+        };
+    }
+</script>
+
+<div class='embed-color-pill' style="backgroundColor: {computed !== undefined ? computed : ''}"/> -->
---
+++ src/components/EmbedImage.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let url = null;
+    
+</script>
+
+{#if url}
+    <a className='embed-thumbnail embed-thumbnail-rich'>
+        <img className='image' role='presentation' src={url}/>
+    </a>
+{/if}
